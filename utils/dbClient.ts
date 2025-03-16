b47a62dbf76037240ce98396793517e87bb087b6
nirme <meirnir89@gmail.com>
2025-03-16 10:12:01 +0200

    refactor: remove transaction method from Database class

--- utils/dbClient.ts
+++ utils/dbClient.ts
@@ -16,25 +16,4 @@ export class Database {
       client.release();
     }
   }
-
-  static async transaction(queries: { text: string; params: any[] }[]) {
-    const pool = DatabaseConfig.getInstance();
-    const client = await pool.connect();
-    
-    try {
-      await client.query("BEGIN");
-      const results = [];
-      for (const query of queries) {
-        results.push(await client.query(query.text, query.params));
-      }
-      await client.query("COMMIT");
-      return results.map(res => res.rows);
-    } catch (error) {
-      await client.query("ROLLBACK");
-      Logger.error("Transaction Failed", error);
-      throw error;
-    } finally {
-      client.release();
-    }
-  }
 }
